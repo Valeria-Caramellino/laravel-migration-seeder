4f7f2952185fe09bf8608c41eed538e4ab67e9a7
valeria caramellino <valeriacaramellino@gmail.com>
2023-06-29 17:44:39 +0200

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,29 @@
     <h1>Welcome Page</h1>
     <div class="row g-4">
         <div class="col">
-            <div>
-               <ul>
-                @foreach ($trains as $item)
-                    <li>{{$trains->azienda}}</li>
-                @endforeach
-               </ul>
+            
+            
+            @foreach ($trains as $item)
+            <div class="card col-3 mx-4 my-2">
+                <div class="card-body">
+                    <h5 class="card-title">{{$item->azienda}}</h5>
+                    <p class="card-text">Stazione di partenza:{{$item->stazione_partenza}}</p>
+                    <p class="card-text">Stazione arrivo:{{$item->stazione_arrivo}}</p>
+                    <small class="card-text">Orario Partenza:{{$item->orario_partenza}}</small>
+                    <p><small class="card-text">Orario Arrivo:{{ $item->orario_arrivo }}</small> </p>
+                    <p class="card-text">Num carrozze: {{$item->num_carrozze}} </p>
+                    <p class="card-text">In orario: {{$item->in_orario}} </p>
+                    <p class="card-text">In cancellazione: {{$item->cancellato}} </p>
+                    <div>
+                            <a href="/" class="card-link">Card link</a>
+                            <a href="/otherpage" class="card-link">Another link</a>
+                    </div>
+                  
+                </div>
             </div>
+            @endforeach
+               
+            
         </div>
     </div>
 
