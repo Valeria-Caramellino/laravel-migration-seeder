8b0d2585dd5feb65bd0dfa1f0229d5f282d9c7e2
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-03 11:06:15 +0200

    fix

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -18,15 +18,15 @@ class TrainsTableSeeder extends Seeder
     {
         for ($i= 0; $i < 10; $i++){
             $newTrains = new Train();
-            $newTrains ->azienda = $faker->sentence(2);
-            $newTrains->stazione_partenza = $faker->sentence(2);            
-            $newTrains->stazione_arrivo = $faker->sentence(2);            
+            $newTrains ->azienda = $faker->company();
+            $newTrains->stazione_partenza = $faker->city();            
+            $newTrains->stazione_arrivo = $faker->city();           
             $newTrains->orario_partenza = $faker->time();           
             $newTrains->orario_arrivo = $faker->time();            
-            $newTrains->codice_treno = $faker->sentence(2);            
+            $newTrains->codice_treno = $faker->regexify('[A-Z]{5}[0-4]{3}');            
             $newTrains->num_carrozze = $faker->randomNumber(3, false);        
-            $newTrains->in_orario = $faker->sentence(2);            
-            $newTrains->cancellato = $faker->sentence(2);            
+            $newTrains->in_orario = $faker->boolean();    
+            $newTrains->cancellato = $faker->boolean();            
             $newTrains->data_partenza = $faker->date();
             $newTrains->save();
         }
